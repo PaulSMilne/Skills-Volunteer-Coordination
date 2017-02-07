a407d403358318cdae839eb37e8fb47d4cbacff8
Paul Milne <hallhill@gmail.com>
2017-02-07 16:28:41 +0000

    seeded the db with project and skills, and migrated pending migrations. Then added a nested api request onto the projects page to get a set of projects with associated skills.

--- react/client/src/components/ProjectsList.jsx
+++ react/client/src/components/ProjectsList.jsx
@@ -6,7 +6,10 @@ constructor(props){
      super(props)
      this.state = { projects: []}
 }
-
+// There are as many requests as needed to get the full set of objects
+// for each item in the projects list a new request is sent
+// using the id to target an individual project and its skills
+// and put those into the this.state.projects array.
 loadProjects(url){
      const request = new XMLHttpRequest()
      request.open('GET', url)
@@ -17,21 +20,30 @@ loadProjects(url){
      request.onload = () => {
 
           if (request.status===200){
+               let projectSkills = []
                const jsonString = request.responseText
                const projectsList = JSON.parse(jsonString)
-
-               const projectsIDs = []
-
                for (let project of projectsList){
-                 projectsIDs.push(project.id)
+                 let newUrl = url+"/"+project.id
+                 const newRequest = new XMLHttpRequest()
+                 newRequest.open('GET', newUrl)
+                 newRequest.onload = () => {
+                   if (newRequest.status===200){
+                     const newJsonString = newRequest.responseText
+                     const projectWithSkills = JSON.parse(newJsonString)
+                     projectSkills.push(projectWithSkills)
+                   }
+                 }
+                 newRequest.send()
+//                 projectsIDs.push(project.id)
                }
-
-               this.setState({projects:projectsIDs})
+               this.setState({projects:projectSkills})
                console.log(this.state.projects)
           }
      }
      request.send()
 }
+
 
 componentDidMount(){
      this.loadProjects('http://localhost:5000/api/projects')
