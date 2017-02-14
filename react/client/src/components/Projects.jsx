e642a2b9b59ebbe19a49d3b9d5f707d0ebc69a2e
Paul Milne <hallhill@gmail.com>
2017-02-14 21:49:38 +0000

    buggy

--- react/client/src/components/Projects.jsx
+++ react/client/src/components/Projects.jsx
@@ -18,64 +18,18 @@ makeRequest(url, callback){
 }
 
 projectRequestComplete(response){
-  //if (this.status !==200) return;
        const jsonString = response
        const projectList = JSON.parse(jsonString)
        console.log(projectList)
-       console.log(response)
-      //  for (let project of projectList){
-      //    let newUrl = url+"/"+project.id
-      //    makeRequest(newUrl, skillRequestComplete)
-      //    }
   }
 
 skillRequestComplete(){
-  if (this.request.status===200){
     let projectSkills = []
     const newJsonString = this.request.responseText
     const projectWithSkills = JSON.parse(newJsonString)
     projectSkills.push(projectWithSkills)
     this.setState({projects:projectSkills})
-  }
 }
-
-// addNewProject(projectData) {
-//   const currentProjects = this.state.projects;
-//   const newProjects = […this.state.projects, projectData]
-//   this.setState({
-//      projects: newProjects
-//   })
-// }
-
-// loadProjects(url){
-//      const request = new XMLHttpRequest()
-//      request.open('GET', url)
-//      request.onload = () => {
-//
-//           if (request.status===200){
-//                let projectSkills = []
-//                const jsonString = request.responseText
-//                const projectList = JSON.parse(jsonString)
-//                for (let project of projectList){
-//                  let newUrl = url+"/"+project.id
-//                  const newRequest = new XMLHttpRequest()
-//                  newRequest.open('GET', newUrl)
-//                  newRequest.onload = () => {
-//                    if (newRequest.status===200){
-//                      const newJsonString = newRequest.responseText
-//                      const projectWithSkills = JSON.parse(newJsonString)
-//                      projectSkills.push(projectWithSkills)
-//                      this.setState({projects:projectSkills})
-//                    }
-//                  }
-//                  newRequest.send()
-//                }
-//
-//           }
-//      }
-//      request.send()
-// }
-
 
 componentDidMount(){
      this.makeRequest('http://localhost:5000/api/projects', this.projectRequestComplete)
